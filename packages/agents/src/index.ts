41c96cdb967244c6669b9638411a72918029b3f9
SirPopGhost <47957443+SirPopGhost@users.noreply.github.com>
2026-05-31 15:30:45 -0700

    Added tools

--- packages/agents/src/index.ts
+++ packages/agents/src/index.ts
@@ -7,6 +7,15 @@ export * from "./callAgent";
 export * from "./pricingAgent";
 export * from "./recordsAgent";
 export * from "./tools";
+export {
+  auditRecordsTransfer,
+  buildRecordsTransferPacket,
+  formatOpseraAuditReason,
+  type OpseraAuditResult,
+  type OpseraAuditStatus,
+  type RecordsTransferPacket,
+  type RecordsTransferRecord
+} from "./tools/opsera";
 export * from "./guardrails";
 export * from "./mockProvider";
 export * from "./e2bRunner";
